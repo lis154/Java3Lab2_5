4f06b4ed5e4b8913686020d1a50f4942fd0d3b6f
lis154 <lis154@mail.ru>
2018-08-16 22:51:33 +0300

    Initial commit

---
+++ src/main/java/Students.java
@@ -0,0 +1,11 @@
+import java.io.Serializable;
+
+public class Students implements Serializable {
+    String name;
+    int age;
+
+    public Students(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
